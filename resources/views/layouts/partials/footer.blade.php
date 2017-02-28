d43fe3e1da45bf1b588c6c7dd7961efe6e894928
Edward Flores <Edward_4578@hotmail.com>
2017-02-27 21:32:24 -0400

    modificacion de imagen en administrador

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,12 +1,12 @@
 <!-- Main Footer -->
-<footer class="main-footer ">
+<footer class="main-footer">
 
     <!-- To the right -->
     <div class="row">
         <div class="col-lg-3">
         </div>
         <div class="col-lg-8">
-            <img class="img-responsive" alt="Imagen responsive" src="img/footer1.png" height="100" width="800"/>
+            {{ Html::image('img/footer1.png', 'footer',  array('width'=>'800px','height'=>'100px', 'class'=>'img-responsive')) }}            
         </div>
         <div class="col-lg-3">  
         </div>
